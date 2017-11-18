3af16f67a13e69c5cc5a8a249fae15fdf70fc205
Stephen Timko <stephen@jet.com>
2017-11-18 13:54:21 -0500

    razz

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ server
     <head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta charSet='utf-8' />
-        <title>Welcome to Razzleeeeeee</title>
+        <title>Welcome to Razz a mataz</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${assets.client.css
           ? `<link rel="stylesheet" href="${assets.client.css}">`
