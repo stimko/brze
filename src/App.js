7d67af1d3ea101794e5a9f0bcf1529a149787f9d
Stephen Timko <stephen@jet.com>
2017-11-20 00:39:57 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,60 @@
-import './App.css';
+import "./reset.css";
+import "./fonts.css";
+import "./App.css";
+import SignUp from "./SignUp/SignUp";
 
-import React from 'react';
-const App = () => <div>Welcome to Razzle.</div>;
+import React from "react";
+
+const num = n => <span className="num">{n}</span>;
+
+const App = () => (
+  <div>
+    <img className="brzeLogo" alt="Brze" src="/images/brze.png" />
+    <h1>
+      <span className="makingPickupEasy">Making pickup easy! </span>
+      <span className="carriers">FedEx • USPS • UPS</span>
+    </h1>
+    <div className="intro">
+      <div className="title">
+        <span className="fontWeight500">Introducing Brze. </span>
+        <span>Returns Made Easy!</span>
+      </div>
+      <div className="finally">
+        Finally, an affordable service that will pick up your package/s from
+        your home and drop them off at
+        <span className="carriers"> FedEx, USPS, or UPS.</span>
+      </div>
+    </div>
+    <div className="easy">
+      <div className="verticalBrze">Brze</div>
+      <div className="steps">
+        <span>
+          It’s as simple as
+          <span className="fontWeight500"> 1, 2, 3</span>
+          <span>!</span>
+        </span>
+        <div className="numWrapper">
+          {num("1 ")}
+          <span className="fontWeight500">TEXT US </span>
+          <span>at</span>
+          <span className="fontWeight500"> 848.702.3698</span>
+        </div>
+        <div className="numWrapper">
+          {num("2 ")}
+          <span>We come and pickup your packages*</span>
+        </div>
+        <div className="numWrapper">
+          {num("3 ")}
+          <span>
+            We text you a confirmation that inclides an image and tracking
+            number after delivery
+          </span>
+        </div>
+        <div className="currently"> *Currently in Summit, NJ Only</div>
+      </div>
+    </div>
+    {SignUp()}
+  </div>
+);
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,49 @@
-import App from './App';
-import React from 'react';
-import express from 'express';
-import { renderToString } from 'react-dom/server';
+import App from "./App";
+import React from "react";
+import express from "express";
+import { renderToString } from "react-dom/server";
 
+const bodyParser = require('body-parser');
+const { Client } = require('pg');
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+const client = new Client({ssl: true});
+
+const startClient = async () => {
+  await client.connect();
+}
+startClient();
 
 const server = express();
-
 server
-  .disable('x-powered-by')
+  .disable("x-powered-by")
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', (req, res) => {
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .post('/api/signup', (req, postRes) => {
+    console.log(req.body);
+    const query = {
+      name: 'fetch-breezer',
+      text: 'SELECT * FROM breezers WHERE phone = $1',
+      values: [req.body.phone]
+    }
+
+    client.query(query)
+      .then(res => {
+        console.log("rows", res.rows);
+        if(!res.rows.length){
+          const query = {
+            name: 'write-breezer',
+            text: 'INSERT INTO breezers (phone, name, address, addressoptional, zip, email, city, password, state) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+            values: [req.body.phone, req.body.name, req.body.address, req.body.addressoptional, req.body.zip, req.body.email, req.body.city, req.body.password, req.body.state]
+          }
+          client.query(query).then(res => {
+            postRes.send(res);
+          }).catch(e => console.log("Write Failure", e))
+        }
+      })
+      .catch(e => console.error("Get Failure", e));
+  })
+  .get("/*", (req, res) => {
     const markup = renderToString(<App />);
     res.send(
       `<!doctype html>
@@ -22,8 +55,8 @@ server
         <meta name="viewport" content="width=device-width, initial-scale=1">
         ${assets.client.css
           ? `<link rel="stylesheet" href="${assets.client.css}">`
-          : ''}
-         ${process.env.NODE_ENV === 'production'
+          : ""}
+         ${process.env.NODE_ENV === "production"
            ? `<script src="${assets.client.js}" defer></script>`
            : `<script src="${assets.client.js}" defer crossorigin></script>`}
     </head>
