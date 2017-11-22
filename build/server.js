535c659bf12e37cfd5a77e739feaa80e8d09ca94
Stephen Timko <stephen@jet.com>
2017-11-22 14:30:40 -0500

    process

--- build/server.js
+++ build/server.js
@@ -974,7 +974,7 @@ if (false) {
   console.info('✅  Server-side HMR Enabled!');
 }
 
-var port = proccess.env.PORT;
+var port = process.env.PORT;
 
 /* harmony default export */ __webpack_exports__["default"] = (__WEBPACK_IMPORTED_MODULE_0_express___default()().use(function (req, res) {
   return __WEBPACK_IMPORTED_MODULE_1__server__["a" /* default */].handle(req, res);
