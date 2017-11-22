088a50584b60932ef2b3fdae4e97c4974bb8bd1e
Stephen Timko <stephen@jet.com>
2017-11-22 17:27:32 -0500

    test

--- build/server.js
+++ build/server.js
@@ -1086,7 +1086,7 @@ var sendSms = function sendSms(num, res, msg) {
 console.log("/Users/stephentimko/Documents/projects/brze/build/public");
 
 var server = __WEBPACK_IMPORTED_MODULE_4_express___default()();
-server.disable("x-powered-by").use(__WEBPACK_IMPORTED_MODULE_4_express___default.a.static("https://brze.herokuapp.com/build/public")).use(bodyParser.urlencoded({ extended: true })).use(bodyParser.json()).post('/api/text', function (req, postRes) {
+server.disable("x-powered-by").use(__WEBPACK_IMPORTED_MODULE_4_express___default.a.static(path.join(__dirname, 'public'))).use(bodyParser.urlencoded({ extended: true })).use(bodyParser.json()).post('/api/text', function (req, postRes) {
   pgClient.query(findUserByNumberQuery(req.param('from'))).then(function (res) {
     var message = res.rows.length ? 'Welcome to Brze! Please check back soon for beta!' : 'Welcome to Brze! Please register an account at brze.io and check back for beta!';
     sendSms(req.param('from'), postRes, message);
